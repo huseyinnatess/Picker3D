f8b8b613e6ebc92f61fcfbf512c610a93e39a864
huseyinnatess <ateshuseyin247@gmail.com>
2024-10-07 13:48:00 +0300

    Input system finalized.

---
+++ Assets/Scripts/Data/ValueObjects/InputData.cs
@@ -0,0 +1,10 @@
+﻿namespace Data.ValueObjects
+{
+    [System.Serializable]
+    public struct InputData
+    {
+        public float HorizontalInputSpeed;
+        public UnityEngine.Vector2 ClampValues;
+        public float ClampSpeed;
+    }
+}
---
+++ Assets/Scripts/Keys/HorizontalInputParams.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Mathematics;
+
+namespace Keys
+{
+    public struct HorizontalInputParams
+    {
+        public float2 ClampValues;
+    }
+}
